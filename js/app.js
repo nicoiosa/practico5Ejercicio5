4f4259fc1d6d926b926997b07616be62ed85531e
nicoIosa <nicolasiosa@hotmail.com>
2023-11-23 01:15:58 -0300

    arreglado boton resume

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 let startTime;
 let stopwatchInterval;
 let pausedTime = 0;
+let isRunning;
 
 function start() {
-  startTime = new Date().getTime() - pausedTime;
-  stopwatchInterval = setInterval(display, 1);
-  
+  if (!isRunning) {
+    startTime = new Date().getTime() - pausedTime;
+    stopwatchInterval = setInterval(display, 1);
+    btns[0].innerText = "Reanudar";
+    btns[0].removeEventListener("click", start);
+    btns[0].addEventListener("click", resume);
+    isRunning = true;
+  }
+}
+
+function resume() {
+  if (!isRunning) {
+    stopwatchInterval = setInterval(display, 1);
+    isRunning = true;
+  }
 }
 
 function pause() {
   clearInterval(stopwatchInterval);
   pausedTime = new Date().getTime() - startTime;
   stopwatchInterval = null;
+  isRunning = false;
 }
 
 function reset() {
@@ -19,6 +33,9 @@ function reset() {
   pausedTime = 0;
   const clock = document.querySelector("h5");
   clock.innerHTML = "00 : 00.00";
+  btns[0].innerText = "Inciar";
+  btns[0].removeEventListener("click", resume);
+  btns[0].addEventListener("click", start);
 }
 
 function display() {
